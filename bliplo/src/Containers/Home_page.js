89ccb087292053c852aef6a1e6f94b29f57e4da9
logicfirst <slee@logicfirstllc.com>
2021-06-27 15:28:20 -0400

    06272021-1530-Created basic Home page

---
+++ bliplo/src/Containers/Home_page.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+// import Logo from '../src/Components/Logo'
+
+export default class Home_page extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Bliplo</h1>
+      </div>
+    )
+  }
+}
